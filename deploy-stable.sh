b597eb351c284a611285a33e1fcf97469c35ea4b
Kelvin <41-kelvin@users.noreply.gitlab.futo.org>
2026-03-06 17:43:19 +0000

    Edit deploy-stable.sh

--- deploy-stable.sh
+++ deploy-stable.sh
@@ -39,7 +39,7 @@ echo "Building content..."
 VERSION="$(git describe --tags)"
 
 echo "Deploying music artifacts to Cloudflare R2..."
-upload_apk_latest_and_versioned "./app/build/outputs/apk/stable/release/app-release.apk" "app-release.apk"
+upload_apk_latest_and_versioned "./app/build/outputs/apk/release/app-release.apk" "app-release.apk"
 # upload_apk_latest_and_versioned "./app/build/outputs/apk/stable/release/app-stable-x86_64-release.apk" "app-x86_64-release.apk"
 # upload_apk_latest_and_versioned "./app/build/outputs/apk/stable/release/app-stable-arm64-v8a-release.apk" "app-arm64-v8a-release.apk"
 # upload_apk_latest_and_versioned "./app/build/outputs/apk/stable/release/app-stable-armeabi-v7a-release.apk" "app-armeabi-v7a-release.apk"
