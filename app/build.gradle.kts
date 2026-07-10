bc7ed30b8001b60b704f0574678f21b44933e244
Kelvin <kelvin@futo.org>
2026-07-10 23:13:25 +0200

    Version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,7 +59,7 @@ android {
         applicationId = "com.futo.music"
         minSdk = 29
         targetSdk = 36
-        versionCode = 30
+        versionCode = 31
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
