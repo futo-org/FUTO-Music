1662cf6ec7658e351a7138520d4528f0487fd465
Kelvin <kelvin@futo.org>
2026-08-31 17:57:52 +0200

    Remove install packages permission for play store

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,7 +58,7 @@ android {
         applicationId = "com.futo.music"
         minSdk = 29
         targetSdk = 36
-        versionCode = 43
+        versionCode = 44
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
